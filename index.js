3fe3d17a35613ea9a72f4ebb48650efd792088d0
nicosantarin <nicosantarin@hotmail.com>
2018-03-05 22:34:20 -0800

    working code
    need env var running

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ const vision = require('@google-cloud/vision');
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
 
-// Imports the Google Cloud client library.
-// const Storage = require('@google-cloud/storage');
-
-// // Instantiates a client. Explicitly use service account credentials by
-// // specifying the private key file. All clients in google-cloud-node have this
-// // helper, see https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/latest/guides/authentication
-// const vvision = vision({
-//   keyFilename: 'C:\Users\Nico\Documents\GitHub\LabA6-4235\Lab6-4235-0fe62e6b33ea.json'
-// });
-
 
 // Performs label detection on the image file
 client
